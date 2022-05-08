6892589dbc9105bdbe86140f330f5f7cee58440b
MackEdweise <edwardsmarc968@gmail.com>
2022-05-07 21:41:50 -0700

    Remove obsolete config

--- analysisservice/database.py
+++ analysisservice/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import urllib
 
-engine = create_engine('postgresql+psycopg2://postgress:tercesdeqmis@35.238.0.219/qedemudb')
+engine = create_engine('postgresql+psycopg2://postgress:<rabbit-password>@35.238.0.219/qedemudb')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
